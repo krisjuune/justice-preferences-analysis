42ebffda2dcabd791f13bf67ec314c6be5314606
krisjuune <kristiina.joon@gmail.com>
2025-01-29 10:30:59 +0100

    fix lpa main plot

---
+++ scripts/plots/lpa_plots.R
@@ -0,0 +1,98 @@
+library(ggplot_profile_principles)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(patchwork)
+library(here)
+
+lpa_data <- read_csv(here("data", "lpa_data.csv"))[, -1] |>
+  filter(!is.na(justice_class)) |>
+  mutate(
+    justice_class = factor(
+        justice_class,
+        levels = c(
+            "1", "3", "2"
+        ), 
+        labels = c(
+            "egalitarian", "universal", "utilitarian"
+        )
+    )
+  )
+
+# calculate scores for each principle
+mean_values <- lpa_data %>%
+  group_by(justice_class) %>%
+  summarize(
+    utilitarian = mean(utilitarian, na.rm = TRUE),
+    egalitarian = mean(egalitarian, na.rm = TRUE),
+    sufficientarian = mean(sufficientarian, na.rm = TRUE),
+    limitarian = mean(limitarian, na.rm = TRUE)
+  )
+
+mean_values_long <- mean_values %>%
+  pivot_longer(cols = utilitarian:limitarian,
+               names_to = "variable",
+               values_to = "value")
+
+mean_values_long <- mean_values_long %>%
+  mutate(
+    ymin = value - 0.95,
+    ymax = value + 0.95
+  )
+
+# Plot principles on x-axis and latent profiles as colors in the legend
+plot_profile_principles <- ggplot(mean_values_long,
+                                  aes(x = variable,
+                                      y = value,
+                                      color = factor(justice_class),
+                                      group = factor(justice_class))) +
+  geom_line(size = 1.5) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = ymin, ymax = ymax), width = 0.2, size = 0.8) +
+  labs(title = "Profiles across justice principles",
+       y = "Mean Score",
+       color = "Latent profile") +
+  theme_classic() +
+  theme(
+    axis.title.x = element_blank()
+  ) +
+  scale_color_viridis_d(end = .8)
+
+# get nr of people in each justice group
+plot_profile_counts <- ggplot(lpa_data,
+                              aes(x = factor(justice_class),
+                                  fill = factor(justice_class))) +
+  geom_bar() +
+  labs(title = "Profile sizes",
+       y = "Count") +
+  theme_classic() +
+  theme(
+    legend.position = "none",
+    axis.title.x = element_blank()
+  ) +
+  scale_fill_viridis_d(end = .8)
+
+
+plot_profile_principles
+plot_profile_counts
+
+plot_profile_counts + plot_profile_principles + plot_layout(ncol = 2)
+
+# Plotting the profiles per participant
+# Pivot the lpa_data into long format for plotting each participant
+participant_long <- lpa_data %>%
+  pivot_longer(cols = justice_general_1:justice_subsidy_4, 
+               names_to = "variable", 
+               values_to = "value")
+
+plot_participants <- ggplot(participant_long, aes(x = variable, y = value, group = ID, color = factor(justice_class))) +
+  geom_line(alpha = 0.1, size = 0.5) +
+  labs(title = "Individual Participant Profiles",
+       x = "Principle",
+       y = "Score",
+       color = "Latent Profile") +
+  theme_minimal() +
+  guides(color = guide_legend(override.aes = list(size = 2)))
+
+# Display the updated plot with thicker legend lines
+plot_participants
