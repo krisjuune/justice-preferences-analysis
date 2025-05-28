3ddb2425e274cbf69b0ffc637d20c537932b1486
krisjuune <kristiina.joon@gmail.com>
2025-05-28 14:34:42 +0100

    add plot for amces of exemptions between subgroups

--- scripts/analysis/mm_exemptions.R
+++ scripts/analysis/mm_exemptions.R
@@ -6,19 +6,44 @@ library(readr)
 library(cregg)
 source("functions/r-assist.R")
 
-df_heat <- read_csv(here("data", "heat-conjoint.csv"))
+df_heat <- read_csv(
+  here("data", "heat_conjoint.csv"),
+  show_col_types = FALSE
+)
 
-# clean
-df_heat <- filter_respondents(df_heat)
-
-# factorise conjoints and variables for subgroup analysis
-df_heat <- factor_conjoint(df_heat, experiment = "heat")
+df_heat <- df_heat |>
+  filter_respondents() |>
+  factor_conjoint(experiment = "heat") |>
+  mutate(push = case_when(
+    ban == "Ban and replace fossil heating" |
+      tax %in% c("100%", "75%", "50%") ~ "strong",
+    ban == "Ban new installations" |
+      tax %in% c("25%") ~ "soft",
+    ban == "No ban" | tax == "0%" ~ "none",
+    TRUE ~ NA_character_
+  )
+  ) |>
+  mutate(
+    push = factor(
+      push,
+      levels = c(
+        "strong",
+        "soft",
+        "none"
+      ),
+      labels = c(
+        "High stringency",
+        "Medium stringency",
+        "Low stringency"
+      )
+    )
+  )
 
 # get MMs
 mm_exemption_rating <- cj(
   df_heat,
   rating ~ tax + ban,
-  id = ~ID,
+  id = ~id,
   estimate = "mm",
   by = ~exemption + justice_class
 )
@@ -26,18 +51,86 @@ mm_exemption_rating <- cj(
 mm_exemption_choice <- cj(
   df_heat,
   Y ~ tax + ban,
-  id = ~ID,
+  id = ~id,
   estimate = "mm",
   by = ~exemption + justice_class
 )
 
-# combine and save to file
-mm_exemption_choice <- mm_exemption_choice |> select(-BY)
-mm_exemption_rating <- mm_exemption_rating |> select(-BY)
-
-mm_exemption_combined <- bind_rows(
-  mm_exemption_choice |> mutate(outcome = "choice"),
-  mm_exemption_rating |> mutate(outcome = "rating")
+# get AMCEs per stringency level
+amce_exemption_rating_str_justice <- cj(
+  df_heat,
+  rating ~ exemption,
+  id = ~id,
+  estimate = "amce",
+  by = ~push + justice_class
 )
 
-write_csv(mm_exemption_combined, here("data", "mm_exemptions.csv"))
+amce_exemption_choice_str_justice <- cj(
+  df_heat,
+  Y ~ exemption,
+  id = ~id,
+  estimate = "amce",
+  by = ~push + justice_class
+)
+
+amce_exemption_rating_general <- cj(
+  df_heat,
+  rating ~ exemption,
+  id = ~id,
+  estimate = "amce",
+  by = ~push
+)
+
+amce_exemption_choice_general <- cj(
+  df_heat,
+  Y ~ exemption,
+  id = ~id,
+  estimate = "amce",
+  by = ~push
+)
+
+# combine and save to file
+mm_exemption_tax_ban <- bind_rows(
+  mm_exemption_choice |>
+    select(-BY) |>
+    mutate(
+      outcome = "choice",
+      subgroups = "justice"
+    ),
+  mm_exemption_rating |> 
+    select(-BY) |>
+    mutate(
+      outcome = "rating",
+      subgroups = "justice"
+    )
+)
+
+amce_exemption_combined <- bind_rows(
+  amce_exemption_choice_str_justice |>
+    select(-BY) |>
+    mutate(
+      outcome = "choice",
+      subgroups = "justice"
+    ),
+  amce_exemption_rating_str_justice |>
+    select(-BY) |>
+    mutate(
+      outcome = "rating",
+      subgroups = "justice"
+    ),
+  amce_exemption_choice_general |>
+    select(-BY) |>
+    mutate(
+      outcome = "choice",
+      subgroups = "general"
+    ),
+  amce_exemption_rating_general |>
+    select(-BY) |>
+    mutate(
+      outcome = "rating",
+      subgroups = "general"
+    )
+)
+
+write_csv(mm_exemption_tax_ban, here("data", "mm_exemptions_tax_ban.csv"))
+write_csv(amce_exemption_combined, here("data", "amce_exemptions.csv"))
--- scripts/plots/exemptions.R
+++ scripts/plots/exemptions.R
@@ -5,12 +5,119 @@ library(readr)
 library(patchwork)
 library(here)
 
-df_plot <- read_csv(here("data", "mm_exemptions.csv")) |>
+df_plot <- read_csv(here("data", "amce_exemptions.csv")) |>
+  filter(outcome == "choice")
+
+df_plot_tax_ban <- read_csv(here("data", "mm_exemptions_tax_ban.csv")) |>
   filter(outcome == "choice", !is.na(justice_class))
 
 main_text_size <- 10
 
 df_plot <- df_plot |>
+  filter(outcome == "choice") |>
+  mutate(
+    push = factor(
+      push,
+      levels = c("Low stringency", "Medium stringency", "High stringency")
+    ),
+    sample = case_when(
+      subgroups == "justice" ~ as.character(justice_class),
+      subgroups == "general" ~ "Overall"
+    ),
+    sample = factor(
+      sample,
+      levels = c("Egalitarianists", "Universalists", "Utilitarianists", "Overall"),
+      labels = c(
+        "Egalitarianists (39.3%)",
+        "Universalists (50.9%)",
+        "Utilitarianists (9.8%)",
+        "Overall"
+      )
+    ),
+    level = factor(
+      level,
+      levels = c(
+        "No exemptions",
+        "Low-income exempted",
+        "Low- and middle-income exempted"
+      ),
+      labels = c(
+        "None",
+        "Low-income",
+        "Low- and middle-income"
+      )
+    )
+  )
+
+plot_exemptions <- function(data){
+  ggplot(data,
+         aes(
+           x = estimate,
+           y = level,
+           colour = sample,
+         )) +
+    geom_point(
+      size = 2.8,
+      shape = "diamond",
+      position = position_dodge(width = 0.5)
+    ) +
+    geom_errorbarh(
+      aes(
+        xmax = upper,
+        xmin = lower
+      ),
+      height = .3,
+      position = position_dodge(width = 0.5)
+    ) +
+    facet_grid(
+      rows = vars(sample),
+      cols = vars(push)
+    )+
+    geom_vline(
+      xintercept = 0,
+      linetype = 2,
+      colour = "gray40",
+      size = .3
+    ) +
+    scale_color_manual(
+      values = c(
+        "Egalitarianists (39.3%)" = viridis::viridis(3, end = .8)[1],
+        "Universalists (50.9%)" = viridis::viridis(3, end = .8)[2],
+        "Utilitarianists (9.8%)" = viridis::viridis(3, end = .8)[3],
+        "Overall" = "gray50"
+      ),
+      name = "Justice orientation"
+    ) +
+    labs(
+      y = NULL,
+      x = "Marginal means"
+    ) +
+    theme_classic() +
+    theme(
+      legend.position = "right",
+      text = element_text(size = 11),
+      strip.background = element_rect(size = 0),
+      strip.text.x = element_text(size = 11, face = "bold")
+    ) +
+    ggtitle("Exempted households")
+}
+
+plot_exemptions <- df_plot |>
+  filter(push != "Low stringency") |>
+  plot_exemptions() +
+  theme(
+    strip.background = element_blank(),
+    strip.text.y = element_blank()
+  )
+
+ggsave(
+  plot = plot_exemptions,
+  here("output", "amce_exemptions_plot.png"),
+  height = 7, width = 10
+)
+
+# also make the plot for tax and ban separately
+df_plot_tax_ban <- df_plot_tax_ban |>
   filter(feature %in% c("tax", "ban")) |>
   filter(!level %in% c("25%", "75%")) |>
   mutate(
@@ -58,7 +165,7 @@ df_plot <- df_plot |>
     )
   )
 
-plot_exemptions <- function(data) {
+plot_exemptions_policy <- function(data) {
   data |>
     ggplot(aes(
       x = estimate,
@@ -68,7 +175,7 @@ plot_exemptions <- function(data) {
       alpha = exemption
     )) +
     geom_point(
-      aes(size = justice_class),
+      size = 2,
       position = position_dodge(width = 0.5)
     ) +
     geom_errorbarh(
@@ -98,26 +205,18 @@ plot_exemptions <- function(data) {
       alpha = "Exemptions applied to",
       y = NULL,
       x = "Marginal means"
-    ) +
-    scale_size_manual(
-      values = c(
-        "Egalitarianists" = 1.85,
-        "Universalists" = 2.2,
-        "Utilitarianists" = 0.85
-      ),
-      guide = "none"
     )
 }
 
-exemption_ban_plot <- df_plot |>
+exemption_ban_plot <- df_plot_tax_ban |>
   filter(feature == "Ban") |>
-  plot_exemptions()
+  plot_exemptions_policy()
 
-exemption_tax_plot <- df_plot |>
+exemption_tax_plot <- df_plot_tax_ban |>
   filter(feature == "Tax") |>
-  plot_exemptions()
+  plot_exemptions_policy()
 
-mm_exemptions_plot <- (exemption_ban_plot / exemption_tax_plot) +
+mm_exemptions_tax_ban_plot <- (exemption_ban_plot / exemption_tax_plot) +
   plot_layout(guides = "collect", axis_titles = "collect") +
   plot_annotation(tag_levels = "A") &
   scale_x_continuous(limits = c(.19, .81)) &
@@ -130,7 +229,7 @@ mm_exemptions_plot <- (exemption_ban_plot / exemption_tax_plot) +
   )
 
 ggsave(
-  plot = mm_exemptions_plot,
-  here("output", "mm_exemptions_plot.png"),
+  plot = mm_exemptions_tax_ban_plot,
+  here("output", "mm_exemptions_tax_ban_plot.png"),
   height = 6, width = 10
 )
